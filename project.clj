4515f0bb44bde33560475e773d3da965e05cc395
Paul Stadig <paul@stadig.name>
2012-12-27 07:00:24 -0500

    Add url and license attributes.

--- project.clj
+++ project.clj
@@ -1,5 +1,9 @@
 (defproject migratus-lein "0.1.0"
   :description "MIGRATE ALL THE THINGS!"
+  :url "http://github.com/pjstadig/migratus-lein"
+  :license {:name "Apache License, Version 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0.html"
+            :distribution :repo}
   :eval-in-leiningen true
   :aliases {"test!" ["do" "clean," "test"]}
   :dependencies [[org.clojure/clojure "1.2.1"]
