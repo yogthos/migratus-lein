c4c697d91ff77022fea3cfb4498365230fefcde8
Paul Stadig <paul@stadig.name>
2012-12-27 05:59:10 -0500

    Use a more recent Clojure version.

--- test-project/project.clj
+++ test-project/project.clj
@@ -1,6 +1,7 @@
 (defproject example "0.1.0"
   :description "Migratus Leiningen plugin example project"
   :dependencies [[org.clojure/clojure "1.2.1"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [mysql/mysql-connector-java "5.1.18"]
                  [migratus "0.6.0-SNAPSHOT"]]
   :plugins [[migratus-lein "0.1.0-SNAPSHOT"]]
