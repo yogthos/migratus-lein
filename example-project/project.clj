d67e5f4eeee108e8a00630408bb18ba8d4440d4f
Paul Stadig <paul@stadig.name>
2012-12-27 06:22:22 -0500

    Bump Migratus dependency to 0.6.0.

--- example-project/project.clj
+++ example-project/project.clj
@@ -2,8 +2,8 @@
   :description "Migratus Leiningen plugin example project"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [mysql/mysql-connector-java "5.1.18"]
-                 [migratus "0.6.0-SNAPSHOT"]]
   :plugins [[migratus-lein "0.1.0-SNAPSHOT"]]
+                 [migratus "0.6.0"]]
   :migratus {:store :database
              :migration-dir "migrations/"
              :db {:classname "com.mysql.jdbc.Driver"
--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :eval-in-leiningen true
   :aliases {"test!" ["do" "clean," "deps," "test"]}
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [migratus "0.6.0-SNAPSHOT"]]
+                 [migratus "0.6.0"]]
   :profiles {:dev {:dependencies [[jar-migrations "1.0.0"]
                                   [log4j "1.2.16"]
-                                  [migratus "0.6.0-SNAPSHOT"]]}})
+                                  [migratus "0.6.0"]]}})
