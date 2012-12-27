5415406475518109f5d0ad453bd500b5f5f7bb38
Paul Stadig <paul@stadig.name>
2012-12-27 06:00:10 -0500

    Rename to example-project.

--- example-project/project.clj
+++ example-project/project.clj
@@ -1,6 +1,5 @@
-(defproject example "0.1.0"
+(defproject example-project "0.1.0"
   :description "Migratus Leiningen plugin example project"
-  :dependencies [[org.clojure/clojure "1.2.1"]
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [mysql/mysql-connector-java "5.1.18"]
                  [migratus "0.6.0-SNAPSHOT"]]
