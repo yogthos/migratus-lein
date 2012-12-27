9270adf8172cc87f97ded55cf2563701ab972333
Paul Stadig <paul@stadig.name>
2012-12-27 06:24:35 -0500

    Dependencies are fetched automatically with Leiningen 2.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject migratus-lein "0.1.0"
   :description "MIGRATE ALL THE THINGS!"
   :eval-in-leiningen true
-  :aliases {"test!" ["do" "clean," "deps," "test"]}
+  :aliases {"test!" ["do" "clean," "test"]}
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [migratus "0.6.0"]]
   :profiles {:dev {:dependencies [[jar-migrations "1.0.0"]
