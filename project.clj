3695868f077d4bf9b919e3a90dcd3720c38e7af3
Paul Stadig <paul@stadig.name>
2012-12-27 05:52:22 -0500

    Clean up dependencies.

--- project.clj
+++ project.clj
@@ -4,10 +4,6 @@
   :aliases {"test!" ["do" "clean," "deps," "test"]}
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [migratus "0.6.0-SNAPSHOT"]]
-  :profiles {:dev {:dependencies [[org.clojure/java.classpath "0.1.0"]
-                                  [org.clojure/java.jdbc "0.2.3"]
-                                  [org.clojure/tools.logging "0.2.3"]
-                                  [jar-migrations "1.0.0"]
+  :profiles {:dev {:dependencies [[jar-migrations "1.0.0"]
                                   [log4j "1.2.16"]
-                                  [robert/bruce "0.7.1"]
                                   [migratus "0.6.0-SNAPSHOT"]]}})
