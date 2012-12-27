98c2d1fcb95dd0aa83ad48885ca65603153cf8e7
Chris Allen <cma@bitemyapp.com>
2012-12-26 20:14:47 -0800

    Update org.clojure
    An ancient java.jdbc version (0.1.0, as opposed to 0.2.3) is breaking things, this is me trying to fix it.
    
    Normally I would've been very unhappy at the old Clojure version, but including only the jdbc version specifically is my attempt to accommodate whatever weird stuff is happening at Sonian.

--- test-project/project.clj
+++ test-project/project.clj
@@ -1,6 +1,7 @@
 (defproject example "0.1.0"
   :description "Migratus Leiningen plugin example project"
   :dependencies [[org.clojure/clojure "1.2.1"]
+                 [org.clojure/java.jdbc "0.2.3"]
                  [mysql/mysql-connector-java "5.1.18"]
                  [migratus "0.6.0-SNAPSHOT"]]
   :plugins [[migratus-lein "0.1.0-SNAPSHOT"]]
