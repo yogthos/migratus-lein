68f81088737426dd2a40fe9268c33852290f1fca
J. Pablo Fernández <pupeno@pupeno.com>
2015-07-01 12:39:06 +0100

    Update the example project to use the current version of migratus-lein, 0.1.1.

--- example-project/project.clj
+++ example-project/project.clj
@@ -1,9 +1,9 @@
-(defproject example-project "0.1.0"
+(defproject example-project "0.1.1"
   :description "Migratus Leiningen plugin example project"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [mysql/mysql-connector-java "5.1.18"]
                  [migratus "0.7.0"]]
-  :plugins [[migratus-lein "0.1.0"]]
+  :plugins [[migratus-lein "0.1.1"]]
   :migratus {:store :database
              :migration-dir "migrations/"
              :db {:classname "com.mysql.jdbc.Driver"
