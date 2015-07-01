91cf47023e4fe67ca4ee62c1902dd8f1de25a2a3
J. Pablo Fernández <pupeno@pupeno.com>
2015-07-01 17:25:17 +0100

    Better description for the project.

--- src/leiningen/migratus.clj
+++ src/leiningen/migratus.clj
@@ -19,7 +19,7 @@
   (:import (java.util.logging Logger Level)))
 
 (defn migratus
-  "MIGRATE ALL THE THINGS!
+  "Maintain database migrations.
 
 Run migrations against a store.  The :migratus key in project.clj is passed to
 migratus as configuration.
