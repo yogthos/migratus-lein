53b5854fedf07f8bb20b3890fb672ab445fa2b35
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2015-10-01 14:13:03 -0400

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject migratus-lein "0.1.7"
+(defproject migratus-lein "0.1."
   :description "Maintain database migrations."
   :url "http://github.com/pjstadig/migratus-lein"
   :license {:name "Apache License, Version 2.0"
@@ -7,5 +7,5 @@
   :eval-in-leiningen true
   :aliases {"test!" ["do" "clean," "test"]}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [migratus "0.8.4"]]
+                 [migratus "0.8.5"]]
   :profiles {:dev {:dependencies [[jar-migrations "1.0.0"]]}})
