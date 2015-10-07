db9caefacc59743fb37bd9ed5ecb1d34d2be68f3
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2015-10-06 20:10:35 -0400

    bumped up migratus

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject migratus-lein "0.1.9"
+(defproject migratus-lein "0.2.0"
   :description "Maintain database migrations."
   :url "http://github.com/pjstadig/migratus-lein"
   :license {:name "Apache License, Version 2.0"
@@ -7,5 +7,5 @@
   :eval-in-leiningen true
   :aliases {"test!" ["do" "clean," "test"]}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [migratus "0.8.6"]]
+                 [migratus "0.8.7"]]
   :profiles {:dev {:dependencies [[jar-migrations "1.0.0"]]}})
